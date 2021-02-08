2a60180ce5e2053de3ce2f5471c09948cc7139bb
B0RJA <borja.latorre@eead.csic.es>
2021-02-08 23:01:12 +0100

    Update data-layer.R

--- R/data-layer.R
+++ R/data-layer.R
@@ -179,8 +179,7 @@ write_data_layer <- function(file, folder, epsg, maxzoom, timeshift = 0)
 				}else{
 					data <- datarange
 				}
-				print(dim(data))
-				#data <- apply(data, 1, rev)
+				data <- data[,dim(x)[2]:1]
 				# warp to mercator
 				m <- array(data[index], dim=dim(index))
 
